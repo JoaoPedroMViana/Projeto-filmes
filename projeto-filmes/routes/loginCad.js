9d089cef8659193676719f5e02c1a7f2d98072ac
joaoPedro <Usurio@DESKTOP-7DSI397.(none)>
2023-11-28 11:43:44 -0300

    extensao handlebars

--- projeto-filmes/routes/loginCad.js
+++ projeto-filmes/routes/loginCad.js
@@ -7,7 +7,7 @@
 //Rotas Login-------------------------------------------------------
     router.get("/login", function(req, res){
         //res.send("ola");//so pode ser usado uma vez na rota
-        res.render("/forms/form_login");
+        res.render("forms/form_login.handlebars");
         //verificar se o user ja existe, se sim vai direto pro watchlist
     })
 
